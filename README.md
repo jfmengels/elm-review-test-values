18c2d86cdfffa846ba878045ef5de1a969521875
Jeroen Engels <jfm.engels@gmail.com>
2020-12-21 09:51:55 +0100

    Add documentation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # elm-review-test-values
 
-Provides [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rules to REPLACEME.
+Provides [`elm-review`](https://package.elm-lang.org/packages/jfmengels/elm-review/latest/) rules to find misuses of test-only values.
 
 
 ## Provided rules
 
-- [`NoTestValuesInProductionCode`](https://package.elm-lang.org/packages/jfmengels/elm-review-test-values/1.0.0/NoTestValuesInProductionCode) - Reports REPLACEME.
+- [`NoTestValuesInProductionCode`](https://package.elm-lang.org/packages/jfmengels/elm-review-test-values/1.0.0/NoTestValuesInProductionCode) - Reports when functions or values meant to be used only in tests are used in production source code.
 
 
 ## Configuration
@@ -18,7 +18,8 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoTestValuesInProductionCode.rule (NoTestValuesInProductionCode.startsWith "tests_")
+    [ NoTestValuesInProductionCode.rule
+        (NoTestValuesInProductionCode.startsWith "tests_")
     ]
 ```
 
--- src/NoTestValuesInProductionCode.elm
+++ src/NoTestValuesInProductionCode.elm
@@ -1,11 +1,12 @@
 module NoTestValuesInProductionCode exposing
     ( rule
-    , Configuration, endsWith, startsWith
+    , Configuration, startsWith, endsWith
     )
 
 {-|
 
 @docs rule
+@docs Configuration, startsWith, endsWith
 
 -}
 
@@ -18,33 +19,139 @@ import Review.Rule as Rule exposing (Error, Rule)
 
 {-| Reports when functions or values meant to be used only in tests are used in production source code.
 
-A recurring question around opaque types
-
     config =
-        [ NoTestValuesInProductionCodeTest.rule (NoTestValuesInProductionCodeTest.endsWith "_TESTS_ONLY")
+        [ NoTestValuesInProductionCodeTest.rule
+            (NoTestValuesInProductionCodeTest.startsWith "test_")
 
         -- or
-        , NoTestValuesInProductionCodeTest.rule (NoTestValuesInProductionCodeTest.startsWith "test_")
+        , NoTestValuesInProductionCodeTest.rule
+            (NoTestValuesInProductionCodeTest.endsWith "_TESTS_ONLY")
         ]
+
+A recurring question around opaque types, is how do you restrict access to constructors so that you can't misuse a data
+type, while also being able to use it in tests in order to do meaningful tests.
+
+
+## Problematic example
+
+In the following example, we have two user roles in an opaque `Role`. `Admin` is being protected so that the only way to create one is by
+having the server return that the user has that role.
+
+    -- module Role exposing (Role, requestRole)
+
+
+    import Http
+    import Json.Decode as Decode exposing (Decoder)
+
+    type Role
+        = User
+        | Admin
+
+    requestRole : (Role -> msg) -> String -> Cmd msg
+    requestRole onResponse id =
+        Http.get
+            { url = "https://server.com/user/" ++ id
+            , expect = Http.expectJson onResponse roleDecoder
+            }
+
+    roleDecoder : Decoder Role
+    roleDecoder =
+        Decode.field "role" Decode.string
+            |> Decode.andThen
+                (\role ->
+                    case role of
+                        "admin" ->
+                            Decode.succeed Admin
+
+                        "user" ->
+                            Decode.succeed User
+
+                        "admin" ->
+                            Decode.fail "Not a valid role"
+                )
+
+With this approach, we have a good foundation to build on, in the sense that it won't be possible for someone to have an
+`Admin` role without the server's consent (the approach could be improved, but for this example it is sufficient).
+
+The problem is that we know won't be able to write tests that require a role, because it is not possible to construct
+such a value in our tests, as we would need to make a HTTP request which `elm-test` doesn't support.
+
+The common solution is therefore to either expose the `Role` (exposing `Role(..)`) or to expose functions to construct
+`Role`.
+
+    -- module Role exposing (Role, admin, requestRole, user)
+    type Role
+        = User
+        | Admin
+
+    user =
+        User
+
+    admin =
+        Admin
+
+Now the problem is that we lost the foundation we had, where a user could be `Admin` only if the server said they were.
+Now such a case would only be caught during code review where the reviewer would have to make sure the role is never
+abused.
+
+
+## Proposed solution
+
+The solution I would go for in the previous example would be to expose functions to construct `Role`, but tag them in a
+way that we should only use them in test code. And using this `elm-review` rule, we'd get a guarantee that this is the
+case.
+
+Those functions are tagged as test-only by their name. You can choose to either have them prefixed or suffixed by a
+set string of your choosing.
 
 
 ## Fail
 
-    a =
-        "REPLACEME example to replace"
+    -- NoTestValuesInProductionCodeTest.startsWith "test_"
+    grantAdminRights user =
+        { user | role = Role.test_admin }
+
+    -- NoTestValuesInProductionCodeTest.endsWith "_TESTS_ONLY"
+    grantAdminRights user =
+        { user | role = Role.admin_TESTS_ONLY }
 
 
 ## Success
 
-    a =
-        "REPLACEME example to replace"
+    -- module RoleTest exposing (roleTest)
+    roleTest =
+        Test.describe "Role"
+            [ Test.test "admins should be able to delete database " <|
+                \() -> Expect.true (canDeleteDatabase Role.test_admin)
+            , Test.test "users should not be able to delete database " <|
+                \() -> Expect.false (canDeleteDatabase Role.test_user)
+            ]
+
+Values marked as test-only can be used in the declaration of other test values.
+
+    -- module User exposing (test_admin_user)
+    test_admin_user =
+        { id = "001"
+        , role = Role.test_admin
+        }
+
+
+## Critique
+
+Is this a perfect solution? No. Tagging values is a brittle solution, and it's easy to misname a function (`testadmin`
+instead of `test_admin` for instance) and lose the guarantees this rule was given you.
+
+It's not perfect, but I think this brittleness will not be a problem in practice, because even if the rule doesn't
+enforce anything, having something called `testXyz` will likely be good enough to prevent misuse in practice (otherwise
+I don't understand why there so many frameworks would prone "convention over configuration" in other ecosystems).
 
 
 ## When (not) to enable this rule
 
 This rule is useful only if you have instances where you wish to add guarantees to the usage of your data types, but
 need to access internals in the context of your tests.
-This rule is only useful when you have a convention in your team or project around naming conventions TODO.
+Also, for this rule to work well, the naming convention for test-only values needs to be communicated to the rest of the
+team or project.
 
 
 ## Try it out
@@ -54,6 +161,8 @@ You can try this rule out by running the following command:
 ```bash
 elm-review --template jfmengels/elm-review-test-values/example --rules NoTestValuesInProductionCodeTest
 ```
+
+The example this rule uses are the two configurations linked to at the top of the page.
 
 -}
 rule : Configuration -> Rule
@@ -69,19 +178,25 @@ rule configuration =
         |> Rule.fromModuleRuleSchema
 
 
+{-| Configure how values should be tagged.
+-}
 type Configuration
-    = EndsWith String
-    | StartsWith String
+    = StartsWith String
+    | EndsWith String
 
 
-endsWith : String -> Configuration
-endsWith =
-    EndsWith
-
-
+{-| A test-only value's name starts with the given string.
+-}
 startsWith : String -> Configuration
 startsWith =
     StartsWith
+
+
+{-| A test-only value's name ends with the given string.
+-}
+endsWith : String -> Configuration
+endsWith =
+    EndsWith
 
 
 type alias Context =
@@ -95,11 +210,11 @@ type alias Context =
 buildTestValuePredicate : Configuration -> String -> Bool
 buildTestValuePredicate configuration =
     case configuration of
-        EndsWith string ->
-            String.endsWith string
-
         StartsWith string ->
             String.startsWith string
+
+        EndsWith string ->
+            String.endsWith string
 
 
 
